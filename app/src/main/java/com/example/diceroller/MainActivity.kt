9e65b46504dd8c65dcc952413ec49d7cc3eeaa8d
Burak337 <burak.alatas2011@gmail.com>
2023-05-13 19:08:05 +0300

    Double dice has been added!

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -19,9 +19,11 @@ class MainActivity : AppCompatActivity() {
             resultTextView.text = "6" */
 
             rollDice()
+            rollSecondDice()
         }
         //we are showing random number in first look of the application
         rollDice()
+        rollSecondDice()
     }
 
     private fun rollDice() {
@@ -43,7 +45,23 @@ class MainActivity : AppCompatActivity() {
         diceImage.setImageResource(drawableResource)
         diceImage.contentDescription = diceRoll.toString()
 
+    }
 
+    private fun rollSecondDice(){
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
